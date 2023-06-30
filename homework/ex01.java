2fffe05c9f575cd55480c0075be172326c1a7799
Andranik <andran1401@gmail.com>
2023-06-30 14:13:25 +0400

    ex01

---
+++ homework/ex01.java
@@ -0,0 +1,18 @@
+package homework;
+
+public class ex01 {
+    public static void main(String[] args) {
+            for (int i = 2; i <1001 ; i++) {
+                if (i%2==0){
+                    System.out.println(i);
+                    
+                }
+    
+            }
+        
+    
+
+        
+    } 
+    
+}
